c362016327b31a1f83e7469362c66d02e9c77ab8
KevinBozic <bozic.kevin@gmail.com>
2016-03-22 11:13:57 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# todos
+
+A Clojure app to ... well, that part is up to you.
+
+## Usage
+
+FIXME
---
+++ src/todos/core.clj
@@ -0,0 +1,16 @@
+(ns todos.core
+  (:gen-class))
+
+(def todos (atom [])) ; holds the todos
+
+(defn -main []
+  (println "Type a to-do and hit enter. Type q to quit.")
+  (loop []
+    (let [text (read-line)]
+      (when (not= text "q")
+        (reset! todos (conj @todos text))
+        (recur))))
+  (println @todos)) 
+      
+     
+ 
